3fbee3dcf515c84c57eccecc18f57d127afa915a
ivan <vanyarock01@gmail.com>
2019-02-25 20:39:31 +0300

    Реализован итератор по классу List, метод print реализован при помощи итератора

--- list.py
+++ list.py
@@ -4,11 +4,21 @@ class List(object):
     def __init__(self, value, next_=None):
         self._value = value
         self.next = next_
+        self.current = self
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current != None:
+            self.current, result = self.current.next, self.current._value
+            return result
+        else:
+            raise StopIteration()
 
     def print(self):
-        print(self._value)
-        if self.next:
-            self.next.print()
+        for item in self:
+            print(item)
 
     def append(self, value):
         if self.next == None:
@@ -17,7 +27,14 @@ class List(object):
             self.next.append(value)
 
 if __name__ == '__main__':
-    ll = List(value=5, next_=List(value=1))
-    ll.print()
-    ll.append(4)
+    ll = List(
+        value=5, next_=List(
+            value=1, next_=List(
+                value=2, next_=List(
+                    value=4, next_=List(
+                        value=9)))))
+    # ll.print()
+    # ll.append(4)
+    ll.append(9)
+    ll.append(11)
     ll.print()
