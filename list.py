8ddeeb3326b1b7c5f2853c8cb77c9d6c1e485180
ivan <vanyarock01@gmail.com>
2019-02-26 18:20:38 +0300

    Добавлен метод reversed(), при помощи него реализован print_reversed

--- list.py
+++ list.py
@@ -3,7 +3,12 @@ class List(object):
 
     def __init__(self, value=None, next_=None):
         self._value = value
-        self.next = next_.copy() if next_ else next_
+        self.prev = None
+        if next_:
+            self.next = next_.copy()
+            self.next.prev = self
+        else:
+            self.next = None
 
     def __iter__(self):
         self.current = self
@@ -16,6 +21,14 @@ class List(object):
         else:
             raise StopIteration()
 
+    def __reversed__(self):
+        current = self
+        while current.next:
+            current = current.next
+        while current:
+            yield current._value
+            current = current.prev
+
     def __add__(self, tail):
         result = self.copy()
         for item in tail:
@@ -24,13 +37,14 @@ class List(object):
 
     def copy(self):
         result = List()
-        curr = result
-        prev = result
+        current = result
+        prev = None
         for item in self:
-            curr._value = item
-            curr.next = List()
-            prev = curr
-            curr = curr.next
+            current._value = item
+            current.prev = prev
+            current.next = List()
+            prev = current
+            current = current.next
         prev.next = None
         return result
 
@@ -38,11 +52,23 @@ class List(object):
         for item in self:
             print(item)
 
+    def print_reversed(self):
+        for item in reversed(self):
+            print(item)
+
     def append(self, value):
         if self.next == None:
             self.next = List(value)
+            self.next.prev = self
         else:
             self.next.append(value)
 
 if __name__ == '__main__':
     # (1, (2, (3, None) ) )
+    sample = List(1)
+    sample += [10, 11, 12]
+    sample += List(9, next_=List(5))
+    sample.print()
+    sample.print_reversed()
+    # for item in reversed(sample):
+    #    print(item)
